68b54123a485e7b7fa15f138a1d4d3005ff85ef7
Herick <herick.tahata@hotmail.com>
2012-11-11 00:11:35 -0200

    Signed-off-by: Herick <herick.tahata@hotmail.com>

---
+++ classic/catalog/language/portuguese-br/module/cart.php
@@ -0,0 +1,10 @@
+<?php
+// Heading 
+$_['heading_title'] = 'Minha Sacola';
+
+// Text
+$_['text_items']    = '%s item(ns) - %s';
+$_['text_empty']    = 'Você ainda não adicionou produtos.';
+$_['text_cart']     = 'Ver Carrinho';
+$_['text_checkout'] = 'Finalizar Pedido';
+?>
